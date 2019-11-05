9d055a9cbab69fd4e74067b3e86167f02c963498
eret9616 <947416983@qq.com>
2019-11-06 02:56:56 +0800

    添加ts-loader

--- index.vue
+++ index.vue
@@ -1,9 +1,11 @@
 <template>
   <div>1</div>
 </template>
-<script>
-export default{
-}
+<script lang='ts'>
+import Vue from 'vue'
+export default Vue.extend({
+
+})
 </script>
 <style>
 </style>
--- webpack.config.js
+++ webpack.config.js
@@ -8,7 +8,15 @@ module.exports ={
           {
             test: /\.vue$/,
             loader: 'vue-loader'
-          }
+          },
+          {
+            test: /\.tsx?$/,
+            loader: 'ts-loader',
+            exclude: /node_modules/,
+            options: {
+              appendTsSuffixTo: [/\.vue$/],
+            }
+          },
         ]
       },
       plugins: [
